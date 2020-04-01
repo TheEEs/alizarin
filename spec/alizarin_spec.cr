1f1542d717b0cbf4f33c934aceab1bb52d6bd8ac
TranDat <visualbasic2013@hotmail.com>
2020-04-02 04:05:42 +0700

    adds test case for WebView#execute_javascript(js_code,&block)

--- spec/alizarin_spec.cr
+++ spec/alizarin_spec.cr
@@ -27,6 +27,17 @@ describe WebView do
     innerHTML = String.new JSC.to_string result
     innerHTML.should eq "<h1>Hello</h1>"
   end
+
+  it "read page's title using WebView#execute_javascript(js,block)" do
+    channel = Channel(String).new
+    WEBVIEW.load_url "http://example.com"
+    wait_for_page_fully_loaded
+    WEBVIEW.execute_javascript "document.title" do |title|
+      t = String.new JSC.to_string title
+      channel.send t
+    end
+    channel.receive.should eq "Example Domain"
+  end
 end
 
 exit 0
