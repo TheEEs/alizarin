c35e36477f3f12c6d058c8fae7bd1f43910cb5a3
TranDat <visualbasic2013@hotmail.com>
2020-04-05 19:01:35 +0700

    updates docs for this module

--- src/web_extension.cr
+++ src/web_extension.cr
@@ -68,11 +68,11 @@ module WebExtension
   # func = function p do
   #   puts typeof(p) # => Array(JSCFunction|JSCObject|JSCPrimative)
   #   # Developers can return anything as long as it has method to_jsc() : JSC::JSValue
-  #   if p.size != 0 
+  #   if p.size != 0
   #     JSCPrimative.new true
   #   else
-  #     false 
-  #   else 
+  #     false
+  #   else
   #   # return type is (JSCPrimative|Bool), it's ok because both JSCPrimative and Bool have method to_jsc()
   # end
   # ```
