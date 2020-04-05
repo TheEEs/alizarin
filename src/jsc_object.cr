63546291bd4f089c387fd662780e0f60a2338caf
TranDat <visualbasic2013@hotmail.com>
2020-04-05 19:05:59 +0700

    adds accessor's functionalities

--- src/jsc_object.cr
+++ src/jsc_object.cr
@@ -1,3 +1,5 @@
+require "./jsc_object/accessor"
+
 # A JSCObject represent a JavaScript object
 struct JSCObject
   @value : JSC::JSValue = Pointer(Void).null
@@ -17,5 +19,29 @@ struct JSCObject
   # :nodoc:
   def initialize(h : JSC::JSValue)
     @value = h
+  end
+
+  # Defines a JavaScript object's property, this is equal to JavaScript `Object.defineProperty`.
+  # See `Accessor` for more. 
+  def mount_accessor(accessor : Accessor)
+    globalObject = JSCContext.get_value "Object"
+    defineProperty = globalObject["defineProperty"].as(JSCFunction)
+    propertyObject = JSCObject.new
+
+    propertyObject["get"] = WebExtension.function p do
+      accessor.get.try &.call
+    end if accessor.get
+
+    propertyObject["set"] = WebExtension.function p do
+      accessor.set.try &.call(p.first)
+      nil
+    end if accessor.set
+
+    defineProperty.call(@value, accessor.name, propertyObject)
+  end
+
+  # Defines a JavaScript object's property, this is equal to JavaScript `Object.defineProperty`. 
+  def define_property(name, value, flags : Accessor::Accessibility)
+    JSC.define_property(@value, name, flags, value.to_jsc)
   end
 end
